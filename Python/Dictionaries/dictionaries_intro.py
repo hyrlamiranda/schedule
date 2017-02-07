e3f83100fafdfce2def5291efcceb98895a492a8
hyrlamiranda <hyrlammiranda@gmail.com>
2017-02-07 14:41:03 -0200

    Learning Dictionaries

---
+++ Python/Dictionaries/dictionaries_intro.py
@@ -0,0 +1,8 @@
+### 07 / 02 / 2017
+### Hyrla Miranda
+### Dictionaries Introduction
+
+book_1 = {'author' : 'hyrla miranda', 'price' : 10}
+
+print(book_1['author'])
+print(book_1['price'])
---
+++ Python/Dictionaries/in_operator.py
@@ -0,0 +1,13 @@
+### 07 / 02 / 2017
+### Hyrla Miranda
+### Using a key to get a value
+
+# Create a dictionary of terms
+terms = {'variable' : 'represents or refers to a value stored in memory' , 'string' : 'A sequence of characters'}
+
+if 'float' in terms:
+	print("I know what a float is.")
+else:
+	print("I do not know what that is.")
+	
+print(terms['variable'])
