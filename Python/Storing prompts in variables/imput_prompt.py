0c467aae9d220a82a99fa8fbd7828f17eec49b13
hyrlamiranda <hyrlammiranda@gmail.com>
2017-02-09 13:45:03 -0200

    Storing prompts in variables

---
+++ Python/Storing prompts in variables/imput_prompt.py
@@ -0,0 +1,7 @@
+### 09 / 02 / 2017
+### Hyrla Miranda
+### Using the input prompt
+
+prompt = " This is our lecture dicussing Python's input prompt and how to keep it tidy.Please enter your favorite lecture so far and why you like it?"
+
+favorite_lecture = input(prompt)
