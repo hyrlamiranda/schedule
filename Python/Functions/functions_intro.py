8c3178a0e8be8e94c0d2d49bd36f21cc12d4308f
hyrlamiranda <hyrlammiranda@gmail.com>
2017-02-22 20:53:34 -0300

    Functions

---
+++ Python/Functions/functions_intro.py
@@ -0,0 +1,9 @@
+### 22/ 02 / 2017
+### Hyrla Miranda
+### How to define a function
+
+def hello_world():
+	"""Displaying hello world"""
+	print("Hello World!")
+
+hello_world()
---
+++ Python/Functions/passing_arguments.py
@@ -0,0 +1,9 @@
+### 22/ 02 / 2017
+### Hyrla Miranda
+### Passing arguments to a function
+
+def hello_world(username):
+	"""Showing a username"""
+	print(" Hello " + username.title() + ".")
+
+hello_world("hyrla Miranda")
